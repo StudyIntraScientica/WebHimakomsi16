57b5fcee197ccb491afcb44346647c6a4a655c4d
faldyif <faldy.if@gmail.com>
2016-07-28 01:37:27 +0700

    Init auth & event n news table

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view("welcome");
 });
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
---
+++ database/migrations/2016_07_26_161031_create_table_blog.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableBlog extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('content');
+            $table->integer('tag');
+            $table->string('image');
+            $table->author('author');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('blogs');
+    }
+}
